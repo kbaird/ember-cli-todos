e7391c4fe218884a9e095f63c442818ffa23927a
Kevin C. Baird <kcbaird@world.oberlin.edu>
2016-09-28 08:52:31 -0700

    Use stock JSONAPISerializer, title now renders in list

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,10 +1,6 @@
 import DS from 'ember-data';
 
 var serializer;
-serializer = DS.JSONSerializer.extend({
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    return this._super(store, primaryModelClass, payload.data, id, requestType);
-  }
-});
+serializer = DS.JSONAPISerializer.extend({});
 
 export default serializer;
